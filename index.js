8188efd74dd59c012b19ed1e04d6cc0ece35048c
Ian Pratt <ianjdpratt@gmail.com>
2022-07-13 19:06:55 -0500

    changes

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-import execa from "execa";
+#!/usr/bin/env node
+import { execa } from "execa";
+import Listr from "listr";
+
+new Listr([
+    {
+        title: "Removing package-lock",
+        task: () => execa("rm", ["package-lock.json"]),
+    },
+    {
+        title: "Running npm install",
+        task: () => execa("npm", ["install"]),
+    },
+    {
+        title: "Adding package-lock to git",
+        task: (ctx, task) =>
+            execa("git", ["add", "package-lock.json"]).catch(() => task.skip()),
+    },
+]).run();
